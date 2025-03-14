3b591f700ae9e6da8f4f345837a7aa7acda24362
justinenorie <justinedelacruz97@gmail.com>
2025-03-14 22:44:14 +0800

    Refactor code style for consistency and improve readability

--- apps/web-app/src/App.jsx
+++ apps/web-app/src/App.jsx
@@ -1,19 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import HomePage from './pages/Home';
-import LoginPage from './pages/Login';
-import RegisterPage from './pages/Register';
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import RegisterPage from "./pages/Register";
 
 const App = () => {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <HomePage/> } />
-        <Route path="login" element={ <LoginPage/> } />
-        <Route path="register" element={ <RegisterPage/> } />
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
       </Routes>
     </div>
-
   );
 };
 
--- apps/web-app/src/pages/Home.jsx
+++ apps/web-app/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-
 
 const HomePage = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
+    <div className="flex h-screen items-center justify-center bg-blue-100">
       <h1 className="text-4xl font-bold text-blue-900">
         Welcome to Our Website!
       </h1>
